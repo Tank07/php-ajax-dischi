a2995f69844c8e51cb40b8efc9e3e8c32fd2ec83
Tancredi Carluccio <tanc_@hotmail.it>
2022-05-10 00:37:55 +0200

    Prima Milestone ma senza estetica

---
+++ index.php
@@ -0,0 +1,95 @@
+<!-- Stampare a schermo una decina di dischi musicali (vedi screenshot).
+Utilizzare:
+Html, CSS, VueJS, axios, PHP
+Prima Milestone:
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+Seconda Milestone:
+Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue.
+Bonus:
+Attraverso un’altra chiamata api, filtrare gli album per genere -->
+
+<?php
+
+    $database = [
+        [
+            'title' => 'New Jersey',
+            'author' => 'Bon Jovi',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+            'genre' => 'Rock'
+        ],
+        [
+            'title' => 'Live at Wembley 86',
+            'author' => 'Queen',
+            'year' => 1992,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Ten\'s Summoner\'s Tales',
+            'author' => 'Sting',
+            'year' => 1993,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Steve Gadd band',
+            'author' => 'Steve Gadd Band',
+            'year' => 2018,
+            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+            'genre' => 'Jazz'
+        ],
+        [
+            'title' => 'Brave new World',
+            'author' => 'Iron Maiden',
+            'year' => 2000,
+            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+            'genre' => 'Metal'
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+            'genre' => 'Rock'
+        ]
+    ];
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax dischi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<style>
+    body{
+        background-color: #1e2d3b;
+    }
+</style>
+</head>
+<body>
+    
+
+    <?php
+    foreach($database as $key => $disco){
+
+        echo "<img class='img-fluid' src='".$disco['poster']."' >";
+        echo "<h2 class='text-white'>" .  $disco['title'] . "</h2>";
+        echo "<p class='text-muted'>" . $disco['author'] . "</p>";
+        echo "<p class='text-muted'>" . $disco['year'] . "</p>";
+        echo "<p class='text-muted d-none'>" . $disco['genre'] . "</p>";
+        
+    }
+
+    ?>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+</body>
+</html>
